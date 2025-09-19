eaac1350c3e0881209040a73d90676f79229a079
movlad <movlgunoev@gmail.com>
2025-09-19 10:12:51 +0300

    Add required properties GlobalUid and User to Box entity

--- MSWMS/Entities/Box.cs
+++ MSWMS/Entities/Box.cs
@@ -4,7 +4,9 @@ public class Box
 {
     public int Id { get; set; }
     public int BoxNumber { get; set; }
+    public required string GlobalUid { get; init; } = Guid.NewGuid().ToString();
     public string? UniqueId { get; set; }
     public required Order Order { get; set; }
+    public required User User { get; set; }
     
 }
