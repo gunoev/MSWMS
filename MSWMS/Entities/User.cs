8ea913efe1df64e21cbe9b0ed203afd073bb2652
movlad <movlgunoev@gmail.com>
2025-09-19 10:12:11 +0300

    Update User entity to replace Email with Username as required field and make Email optional

--- MSWMS/Entities/User.cs
+++ MSWMS/Entities/User.cs
@@ -4,7 +4,8 @@ public class User
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public required string Email { get; set; }
+    public required string Username { get; set; }
+    public string? Email { get; set; }
     public required string PasswordHash { get; set; }
     public required UserStatus Status { get; set; } = UserStatus.Unknown;
     public required ICollection<Role> Roles { get; set; }
