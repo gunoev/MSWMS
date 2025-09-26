70fd1bf5303406a486ddf7a03bc5d9e6042505cb
movlad <movlgunoev@gmail.com>
2025-09-26 11:24:08 +0300

    Add Location entity with properties for orders and identifiers

---
+++ MSWMS/Entities/Location.cs
@@ -0,0 +1,10 @@
+namespace MSWMS.Entities;
+
+public class Location
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Code { get; set; }
+    public ICollection<Order>? OriginOrders { get; set; }
+    public ICollection<Order>? DestinationOrders { get; set; }
+}
