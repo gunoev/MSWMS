a58aba87ed208d5e09c827bc3f382ebdc92511a7
movlad <movlgunoev@gmail.com>
2025-09-26 17:45:18 +0300

    Update Order entity: adjust property modifiers, add `CreatedBy`, and make certain collections optional

--- MSWMS/Entities/Order.cs
+++ MSWMS/Entities/Order.cs
@@ -7,7 +7,7 @@ public class Order
     public required string TransferShipmentNumber { get; set; }
     public required Location Origin { get; set; }
     public required Location Destination { get; set; }
-    public required OrderStatus Status { get; set; } = OrderStatus.New;
+    public OrderStatus Status { get; set; } = OrderStatus.New;
     public DateTime? CollectedDateTime { get; set; }
     public DateTime? LastChangeDateTime { get; set; }
     public DateTime CreatedDateTime { get; init; } = DateTime.Now;
@@ -15,12 +15,13 @@ public class Order
     public required string ShipmentId { get; set; }
     public string? Remark { get; set; }
     public required OrderType Type { get; set; }
+    public required User CreatedBy { get; set; }
     
     public ICollection<User>? CollectedBy { get; set; }
     public required ICollection<Item> Items { get; set; }
-    public required ICollection<Box> Boxes { get; set; }
-    public required ICollection<Scan> Scans { get; set; }
-    public required ICollection<User> Users { get; set; }
+    public ICollection<Box>? Boxes { get; set; }
+    public ICollection<Scan>? Scans { get; set; }
+    public ICollection<User>? Users { get; set; }
     
     
     public enum OrderStatus
