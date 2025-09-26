b584512cd7529e129429b479db7cfea5538dc3d2
movlad <movlgunoev@gmail.com>
2025-09-26 17:45:30 +0300

    Update Role entity: remove `required` modifier from Users collection

--- MSWMS/Entities/Role.cs
+++ MSWMS/Entities/Role.cs
@@ -5,7 +5,7 @@ public class Role
     public int Id { get; set; }
     public string? Name { get; set; }
     public required RoleType Type { get; set; }
-    public required ICollection<User> Users { get; set; }
+    public ICollection<User> Users { get; set; }
     
     public enum RoleType
     {
