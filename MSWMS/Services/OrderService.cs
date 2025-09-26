bcafac101c43cd8e8d74186fed77e887dd59a756
movlad <movlgunoev@gmail.com>
2025-09-26 17:54:04 +0300

    Add `GetByIdAsync` method in OrderService to fetch order by ID

--- MSWMS/Services/OrderService.cs
+++ MSWMS/Services/OrderService.cs
@@ -1,6 +1,15 @@
+using MSWMS.Entities;
+using MSWMS.Repositories;
+
 namespace MSWMS.Services;
 
 public class OrderService
 {
-    
+    private IOrderRepository _orderRepository;
+    public async Task<Order?> GetByIdAsync(int id)
+    {
+        _orderRepository = new OrderRepository(new AppDbContext());
+        
+        return _orderRepository.GetByIdAsync(id).Result;
+    }
 }
