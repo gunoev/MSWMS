ec82e61ea424ce0e0101ad7a36c70abbdfb7b342
movlad <movlgunoev@gmail.com>
2025-09-26 17:45:54 +0300

    Update Item entity: remove `required` modifier from Order property

--- MSWMS/Entities/Item.cs
+++ MSWMS/Entities/Item.cs
@@ -4,6 +4,5 @@ public class Item
 {
     public int Id { get; set; }
     public int NeededQuantity { get; set; }
-    public required Order Order { get; set; }
     public required ICollection<ItemInfo> ItemInfo { get; set; }
 }
