7b17013db02760320b5632e407c2b8aba8004990
movlad <movlgunoev@gmail.com>
2025-09-26 17:46:12 +0300

    Update User entity: add `Location` as a required property

--- MSWMS/Entities/User.cs
+++ MSWMS/Entities/User.cs
@@ -8,6 +8,7 @@ public class User
     public string? Email { get; set; }
     public required string PasswordHash { get; set; }
     public required UserStatus Status { get; set; } = UserStatus.Unknown;
+    public required Location Location { get; set; }
     public required ICollection<Role> Roles { get; set; }
     
     public enum UserStatus
