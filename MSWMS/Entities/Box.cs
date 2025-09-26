b01a83751023c1366b7350f4c5c37659ac9984cb
movlad <movlgunoev@gmail.com>
2025-09-26 17:45:07 +0300

    Update Box entity: remove `required` modifier from Guid property

--- MSWMS/Entities/Box.cs
+++ MSWMS/Entities/Box.cs
@@ -4,7 +4,7 @@ public class Box
 {
     public int Id { get; set; }
     public int BoxNumber { get; set; }
-    public required Guid Guid { get; init; } = Guid.NewGuid();
+    public Guid Guid { get; init; } = Guid.NewGuid();
     public string? UniqueId { get; set; }
     public required Order Order { get; set; }
     public required User User { get; set; }
