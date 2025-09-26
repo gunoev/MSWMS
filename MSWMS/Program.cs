d59fb9fbdae2779ae38aacdefd1a01dde24bbcb9
movlad <movlgunoev@gmail.com>
2025-09-26 17:54:16 +0300

    Configure DI container with services, repositories, and DbContext; replace OpenAPI with Swagger; remove WeatherForecast-related boilerplate

--- MSWMS/Program.cs
+++ MSWMS/Program.cs
@@ -1,41 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using MSWMS.Entities;
+using MSWMS.Repositories;
+using MSWMS.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+// Регистрация AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("TestConnection"))
+);
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<BoxService>();
+builder.Services.AddScoped<ScanService>();
+// Swagger конфигурация
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
