62eaae45ded331855d83fbe5bf40dd41fd1135a5
movlad <movlgunoev@gmail.com>
2025-09-26 11:23:34 +0300

    Update Box entity: replace GlobalUid (string) with Guid type property

--- MSWMS/Entities/Box.cs
+++ MSWMS/Entities/Box.cs
@@ -4,7 +4,7 @@ public class Box
 {
     public int Id { get; set; }
     public int BoxNumber { get; set; }
-    public required string GlobalUid { get; init; } = Guid.NewGuid().ToString();
+    public required Guid Guid { get; init; } = Guid.NewGuid();
     public string? UniqueId { get; set; }
     public required Order Order { get; set; }
     public required User User { get; set; }
