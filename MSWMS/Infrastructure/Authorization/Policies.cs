959ddef4f1a69f0539addfb491bdc7daf7041f7a
gunoev <movlgunoev@gmail.com>
2025-09-28 23:21:09 +0300

    Add `Policies` class for defining authorization policies with roles

---
+++ MSWMS/Infrastructure/Authorization/Policies.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Authorization;
+using MSWMS.Entities;
+
+namespace MSWMS.Infrastructure.Authorization;
+
+public static class Policies
+{
+    public const string RequireAdmin = "RequireAdmin";
+    public const string RequireManager = "RequireManager";
+    public const string RequirePicker = "RequirePicker";
+    
+    public static void AddPolicies(AuthorizationOptions options)
+    {
+        options.AddPolicy(RequireAdmin, policy =>
+            policy.RequireRole(Role.RoleType.Admin.ToString()));
+        
+        options.AddPolicy(RequireManager, policy =>
+            policy.RequireRole(Role.RoleType.Manager.ToString(), Role.RoleType.Admin.ToString()));
+        
+        options.AddPolicy(RequirePicker, policy =>
+            policy.RequireRole(Role.RoleType.Picker.ToString()));
+    }
+}
