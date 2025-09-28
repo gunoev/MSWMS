a027ea66c43c41020c0c6a550ebe2e057f8c72ea
gunoev <movlgunoev@gmail.com>
2025-09-28 16:49:33 +0300

    Add `ShortName` property to `Location` entity

--- MSWMS/Entities/Location.cs
+++ MSWMS/Entities/Location.cs
@@ -4,6 +4,7 @@ public class Location
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public string? ShortName { get; set; }
     public string? Code { get; set; }
     public ICollection<Order>? OriginOrders { get; set; }
     public ICollection<Order>? DestinationOrders { get; set; }
