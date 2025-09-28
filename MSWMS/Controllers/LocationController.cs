8d47c928c55bc1d416fb352fed6b4158ec8ca629
gunoev <movlgunoev@gmail.com>
2025-09-28 16:49:50 +0300

    Add `LocationController` with full CRUD API for managing locations

---
+++ MSWMS/Controllers/LocationController.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSWMS.Entities;
+
+namespace MSWMS.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LocationController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public LocationController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    // GET: api/Location
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Location>>> GetLocations()
+    {
+        return await _context.Locations.ToListAsync();
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Location>> GetLocation(int id)
+    {
+        var location = await _context.Locations.FindAsync(id);
+
+        if (location == null)
+        {
+            return NotFound();
+        }
+
+        return location;
+    }
+    
+    // PUT: api/Location/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutLocation(int id, Location location)
+    {
+        if (id != location.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(location).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!LocationExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+    
+    // POST: api/Location
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Location>> PostLocation(Location location)
+    {
+        _context.Locations.Add(location);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetLocation", new { id = location.Id }, location);
+    }
+    
+    // DELETE: api/Location/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteLocation(int id)
+    {
+        var location = await _context.Locations.FindAsync(id);
+        if (location == null)
+        {
+            return NotFound();
+        }
+
+        _context.Locations.Remove(location);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+    
+    private bool LocationExists(int id)
+    {
+        return _context.Locations.Any(e => e.Id == id);
+    }
+    
+}
