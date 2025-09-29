2ae102c2d3983fca6d5a1820e22806b3d0ece3fa
movlad <movlgunoev@gmail.com>
2025-09-29 17:47:17 +0300

    Inject `IScanService` into `ScanController` and update `PostScan` to process scans using the service.

--- MSWMS/Controllers/ScanController.cs
+++ MSWMS/Controllers/ScanController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using MSWMS.Entities;
 using MSWMS.Models.Requests;
 using MSWMS.Models.Responses;
+using MSWMS.Services.Interfaces;
 
 namespace MSWMS.Controllers;
 
@@ -10,18 +11,28 @@ namespace MSWMS.Controllers;
 public class ScanController : ControllerBase
 {
     private readonly AppDbContext _context;
+    private readonly IScanService _scanService;
 
-    public ScanController(AppDbContext context)
+    public ScanController(AppDbContext context, IScanService scanService)
     {
         _context = context;
+        _scanService = scanService;
     }
 
     // Auth
     [HttpPost]
     public async Task<ActionResult<ScanResponse>> PostScan(ScanRequest scanRequest)
     {
-        // scan
+        var status = await _scanService.ProcessScan(scanRequest);
+        
+        var response = new ScanResponse
+        {
+            Barcode = scanRequest.Barcode, 
+            TimeStamp = DateTime.Now,
+            Status = status,
+            BoxNumber = scanRequest.BoxNumber,
+        };
 
-        return new ScanResponse{Barcode = "123", TimeStamp = DateTime.Now};
+        return response;
     }
 }
