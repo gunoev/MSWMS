9851f76a1caa6979259d84d20c44dcf428e269f4
movlad <movlgunoev@gmail.com>
2025-09-30 16:51:35 +0300

    Add `MappingProfile` to configure mappings between `Order` and `OrderDto` using AutoMapper

---
+++ MSWMS/Infrastructure/MappingProfile.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using MSWMS.Entities;
+using MSWMS.Models.Responses;
+
+namespace MSWMS.Infrastructure;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Order, OrderDto>()
+            .ForMember(dest => dest.Origin, opt => opt.MapFrom(src => src.Origin.Name))
+            .ForMember(dest => dest.Destination, opt => opt.MapFrom(src => src.Destination.Name))
+            .ForMember(dest => dest.OriginCode, opt => opt.MapFrom(src => src.Origin.Code))
+            .ForMember(dest => dest.DestinationCode, opt => opt.MapFrom(src => src.Destination.Code))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedDateTime))
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.LastChangeDateTime))
+            .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy != null ? src.CreatedBy.Name : "Unknown"))
+            .ForMember(dest => dest.TotalBoxes, opt => opt.MapFrom(src => src.Boxes != null ? src.Boxes.Count : 0))
+            .ForMember(dest => dest.TotalQuantity, opt => opt.MapFrom(src => src.Items.Sum(i => i.NeededQuantity)))
+            .ForMember(dest => dest.TotalScanned, opt => opt.MapFrom(src => src.Scans != null ? src.Scans.Count : 0));
+        
+    }
+}
