7d13119b882537ba5bc898a8562eb10a3934c5a6
movlad <movlgunoev@gmail.com>
2025-10-02 15:44:36 +0300

    Inject `AppDbContext` into `UserService` and refactor `GetUserByIdAsync` for dependency injection compliance.

--- MSWMS/Services/UserService.cs
+++ MSWMS/Services/UserService.cs
@@ -1,13 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using MSWMS.Entities;
 
 namespace MSWMS.Services;
 
 public class UserService
 {
+    private readonly AppDbContext _context;
+    
+    public UserService(AppDbContext context)
+    {
+        _context = context;
+    }
+    
     public async Task<User?> GetUserByIdAsync(int userId)
     {
-        await using var db = new AppDbContext();
-        
-        return db.Users.FirstOrDefault(u => u.Id == userId);
+        return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
     }
 }
