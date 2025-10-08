200ec6886c0a6a9ac819d4d81dc6addfa820633e
movlad <movlgunoev@gmail.com>
2025-10-08 17:56:39 +0300

    Add `BoxDto` to standardize box response model

---
+++ MSWMS/Models/DTO/Responses/BoxDto.cs
@@ -0,0 +1,9 @@
+namespace MSWMS.Models.Responses;
+
+public class BoxDto
+{
+    public int Id { get; set; }
+    public string Guid { get; set; }
+    public int BoxNumber { get; set; }
+    public int Quantity { get; set; }
+}
