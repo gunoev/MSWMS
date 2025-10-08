c49c117b842492e8bd70cd213cdc208bfe45519e
movlad <movlgunoev@gmail.com>
2025-10-08 17:57:31 +0300

    Add `BoxId` and `ItemId` properties to `ScanResponse` DTO for enhanced data representation

--- MSWMS/Models/DTO/Responses/ScanResponse.cs
+++ MSWMS/Models/DTO/Responses/ScanResponse.cs
@@ -7,8 +7,10 @@ public class ScanResponse
     public int Id { get; set; }
     public required string Barcode { get; set; }
     public int BoxNumber { get; set; }
+    public int BoxId { get; set; }
     public int OrderId { get; set; }
     public int UserId { get; set; }
+    public int ItemId { get; set; }
     public required string Username { get; set; }
     public required DateTime TimeStamp { get; set; }
     public Scan.ScanStatus Status { get; set; }
