ba79d53b771906e0aef34b2b75c07a467ea64e97
movlad <movlgunoev@gmail.com>
2025-10-16 11:51:30 +0300

    Add `MaxLength` constraints to `Location` properties and mark `Code` as required

--- MSWMS/Entities/Location.cs
+++ MSWMS/Entities/Location.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MSWMS.Entities;
 
 public class Location
 {
     public int Id { get; set; }
+    [MaxLength(256)]
     public required string Name { get; set; }
+    [MaxLength(64)]
     public string? ShortName { get; set; }
-    public string? Code { get; set; }
+    [MaxLength(16)]
+    public required string Code { get; set; }
     public ICollection<Order>? OriginOrders { get; set; }
     public ICollection<Order>? DestinationOrders { get; set; }
 }
