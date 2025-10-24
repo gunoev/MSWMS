9c4d6cfb921d45ec3d31b3cd77a406861c778e72
movlad <movlgunoev@gmail.com>
2025-10-24 17:32:03 +0300

    Add `EventStatus` to `ShipmentEvent` with enum values and apply `MaxLength` validation to `Code` property

--- MSWMS/Entities/ShipmentEvent.cs
+++ MSWMS/Entities/ShipmentEvent.cs
@@ -1,20 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MSWMS.Entities;
 
 public class ShipmentEvent
 {
     public int Id { get; set; }
-    public required string Code { get; set; }
+    [MaxLength(256)] public required string Code { get; set; }
     public DateTime Timestamp { get; set; }
     public required Location Location { get; set; }
     public Box? Box { get; set; }
     public required User User { get; set; }
     public required ShipmentAction Action { get; set; }
-}
+    public required EventStatus Status { get; set; }
 
-public enum ShipmentAction
-{
-    Register,
-    Load,
-    Unload,
-    Check
+    public enum EventStatus
+    {
+        Ok,
+        NotFound,
+        Error,
+    }
+
+    public enum ShipmentAction
+    {
+        Register,
+        Load,
+        Unload,
+        Check
+    }
 }
