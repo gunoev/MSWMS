fe85caa726bbd23cbedbb107cc7b8be2b0821318
movlad <movlgunoev@gmail.com>
2025-10-28 17:52:52 +0200

    Add new role-based authorization policies to support combined role requirements

--- MSWMS/Infrastructure/Authorization/Policies.cs
+++ MSWMS/Infrastructure/Authorization/Policies.cs
@@ -9,7 +9,11 @@ public static class Policies
     public const string RequireManager = "RequireManager";
     public const string RequirePicker = "RequirePicker";
     public const string RequireDispatcher = "RequireDispatcher";
-    public const string LoadingOperator = "LoadingOperator";
+    public const string RequireLoadingOperator = "RequireLoadingOperator";
+    public const string RequireManagerOrDispatcher = "RequireManagerOrDispatcher";
+    public const string RequireManagerOrPicker = "RequireManagerOrPicker";
+    public const string RequireDispatcherOrLoadingOperator = "RequireDispatcherOrLoadingOperator";
+
     
     public static void AddPolicies(AuthorizationOptions options)
     {
@@ -17,15 +21,43 @@ public static class Policies
             policy.RequireRole(nameof(Role.RoleType.Admin)));
         
         options.AddPolicy(RequireManager, policy =>
-            policy.RequireRole(nameof(Role.RoleType.Manager), nameof(Role.RoleType.Admin)));
+            policy.RequireRole(
+                nameof(Role.RoleType.Manager), 
+                nameof(Role.RoleType.Admin)));
         
         options.AddPolicy(RequirePicker, policy =>
-            policy.RequireRole(nameof(Role.RoleType.Picker), nameof(Role.RoleType.Manager), nameof(Role.RoleType.Admin)));
+            policy.RequireRole(
+                nameof(Role.RoleType.Picker), 
+                nameof(Role.RoleType.Manager), 
+                nameof(Role.RoleType.Admin)));
         
         options.AddPolicy(RequireDispatcher, policy =>
-            policy.RequireRole(nameof(Role.RoleType.Dispatcher), nameof(Role.RoleType.Admin)));
+            policy.RequireRole(
+                nameof(Role.RoleType.Dispatcher), 
+                nameof(Role.RoleType.Admin)));
         
-        options.AddPolicy(LoadingOperator, policy =>
-            policy.RequireRole(nameof(Role.RoleType.LoadingOperator), nameof(Role.RoleType.Dispatcher), nameof(Role.RoleType.Admin)));
+        options.AddPolicy(RequireLoadingOperator, policy =>
+            policy.RequireRole(
+                nameof(Role.RoleType.LoadingOperator), 
+                nameof(Role.RoleType.Dispatcher), 
+                nameof(Role.RoleType.Admin)));
+        
+        options.AddPolicy(RequireManagerOrDispatcher, policy =>
+            policy.RequireRole(
+                nameof(Role.RoleType.Manager), 
+                nameof(Role.RoleType.Dispatcher), 
+                nameof(Role.RoleType.Admin)));
+
+        options.AddPolicy(RequireManagerOrPicker, policy =>
+            policy.RequireRole(
+                nameof(Role.RoleType.Manager), 
+                nameof(Role.RoleType.Picker), 
+                nameof(Role.RoleType.Admin)));
+
+        options.AddPolicy(RequireDispatcherOrLoadingOperator, policy =>
+            policy.RequireRole(
+                nameof(Role.RoleType.Dispatcher), 
+                nameof(Role.RoleType.LoadingOperator), 
+                nameof(Role.RoleType.Admin)));
     }
 }
