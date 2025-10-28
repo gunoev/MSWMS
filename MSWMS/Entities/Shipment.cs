400927eb2f1ae08b5dacfb657598215baad891b7
movlad <movlgunoev@gmail.com>
2025-10-28 11:22:06 +0200

    Add `Origin` property to `Shipment` entity to track shipment origin location

--- MSWMS/Entities/Shipment.cs
+++ MSWMS/Entities/Shipment.cs
@@ -5,6 +5,7 @@ public class Shipment
     public int Id { get; set; }
     public required User CreatedBy { get; set; }
     public required ICollection<Order> Orders { get; set; }
+    public required Location Origin { get; set; }
     public required Location Destination { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime Scheduled { get; set; }
