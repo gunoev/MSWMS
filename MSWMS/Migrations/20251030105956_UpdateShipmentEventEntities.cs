09746347ca80d0259d1a25e7c9aa16e25afdf902
movlad <movlgunoev@gmail.com>
2025-10-31 17:49:12 +0200

    Update `ShipmentEvent` entity to add `OrderId` and `ShipmentId1` with relationships and indexes.

---
+++ MSWMS/Migrations/20251030105956_UpdateShipmentEventEntities.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MSWMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateShipmentEventEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "ShipmentEvents",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShipmentId1",
+                table: "ShipmentEvents",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentEvents_OrderId",
+                table: "ShipmentEvents",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentEvents_ShipmentId1",
+                table: "ShipmentEvents",
+                column: "ShipmentId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShipmentEvents_Orders_OrderId",
+                table: "ShipmentEvents",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShipmentEvents_Shipments_ShipmentId1",
+                table: "ShipmentEvents",
+                column: "ShipmentId1",
+                principalTable: "Shipments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShipmentEvents_Orders_OrderId",
+                table: "ShipmentEvents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShipmentEvents_Shipments_ShipmentId1",
+                table: "ShipmentEvents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShipmentEvents_OrderId",
+                table: "ShipmentEvents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShipmentEvents_ShipmentId1",
+                table: "ShipmentEvents");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "ShipmentEvents");
+
+            migrationBuilder.DropColumn(
+                name: "ShipmentId1",
+                table: "ShipmentEvents");
+        }
+    }
+}
