0481ece487506a9228a0f66c27ceaf17697cdd3a
movlad <movlgunoev@gmail.com>
2025-11-07 17:18:16 +0200

    Refactor `ShipmentOrderDto` to include additional fields and map properties from `Order` entity.

--- MSWMS/Models/DTO/Responses/ShipmentOrderDto.cs
+++ MSWMS/Models/DTO/Responses/ShipmentOrderDto.cs
@@ -1,3 +1,5 @@
+using MSWMS.Entities;
+
 namespace MSWMS.Models.Responses;
 
 public class ShipmentOrderDto
@@ -10,11 +12,31 @@ public class ShipmentOrderDto
     public required string Origin { get; set; }
     public required string Destination { get; set; }
     public int Boxes { get; set; }
-    public int TotalQuantity { get; set; }
-    public int TotalScanned { get; set; }
-    public int TotalRemaining { get; set; }
+    public int LoadedBoxes { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalCollectedItems { get; set; }
     public string? Priority { get; set; }
     public string? Status { get; set; }
     public string? Remark { get; set; }
     public string? Type { get; set; }
+    
+    public ShipmentOrderDto FromEntity(Order order)
+    {
+        Id = order.Id;
+        DbCode = order.ShipmentId;
+        TransferOrderNumber = order.TransferOrderNumber;
+        TransferShipmentNumber = order.TransferShipmentNumber;
+        Origin = order.Origin.Name;
+        Destination = order.Destination.Name;
+        Status = order.Status.ToString();
+        Type = order.Type.ToString();
+        Priority = order.Priority.ToString();
+        Boxes = order.Boxes?.Count ?? 0;
+        LoadedBoxes = order.Shipments.SelectMany(s => s.Events)
+            .Count(e => e.Order.Id == order.Id && e.Action == ShipmentEvent.ShipmentAction.Load);
+        TotalItems = order.Items.Sum(i => i.NeededQuantity);
+        TotalCollectedItems = order.Scans?.Count ?? 0;
+
+        return this;
+    }
 }
