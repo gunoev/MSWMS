909df33b637ea2296dd6c39d3e59db06c004229b
movlad <movlgunoev@gmail.com>
2025-11-07 17:18:24 +0200

    Map `Order` entity to `ShipmentOrderDto` with detailed property mappings in `MappingProfile`.

--- MSWMS/Infrastructure/MappingProfile.cs
+++ MSWMS/Infrastructure/MappingProfile.cs
@@ -25,6 +25,23 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.ShortName, opt => opt.MapFrom(src => src.ShortName))
             .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.Code));
+        
+    CreateMap<Order, ShipmentOrderDto>()
+        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+        .ForMember(dest => dest.ShipmentId, opt => opt.Ignore())
+        .ForMember(dest => dest.DbCode, opt => opt.MapFrom(src => src.ShipmentId))
+        .ForMember(dest => dest.TransferOrderNumber, opt => opt.MapFrom(src => src.TransferOrderNumber))
+        .ForMember(dest => dest.TransferShipmentNumber, opt => opt.MapFrom(src => src.TransferShipmentNumber))
+        .ForMember(dest => dest.Origin, opt => opt.MapFrom(src => src.Origin.Name))
+        .ForMember(dest => dest.Destination, opt => opt.MapFrom(src => src.Destination.Name))
+        .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+        .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()))
+        .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority.ToString()))
+        .ForMember(dest => dest.Boxes, opt => opt.MapFrom(src => src.Boxes != null ? src.Boxes.Count : 0))
+        .ForMember(dest => dest.LoadedBoxes, opt => opt.Ignore())
+        .ForMember(dest => dest.TotalItems, opt => opt.MapFrom(src => src.Items.Sum(i => i.NeededQuantity)))
+        .ForMember(dest => dest.TotalCollectedItems, opt => opt.MapFrom(src => src.Scans != null ? src.Scans.Count : 0));
+        
 
     }
 }
