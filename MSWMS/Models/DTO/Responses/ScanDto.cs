d84a6fa21c567c586e3b3395bfdbe0cfab88265b
movlad <movlgunoev@gmail.com>
2025-11-10 11:04:38 +0200

    Add `ScanDto` to represent scan response data in DTO layer

---
+++ MSWMS/Models/DTO/Responses/ScanDto.cs
@@ -0,0 +1,18 @@
+using MSWMS.Entities;
+
+namespace MSWMS.Models.Responses;
+
+public class ScanDto
+{
+    public int Id { get; set; }
+    public required string Barcode { get; set; }
+    public int BoxNumber { get; set; }
+    public int BoxId { get; set; }
+    public int OrderId { get; set; }
+    public int UserId { get; set; }
+    public int ItemId { get; set; }
+    public required string Username { get; set; }
+    public required DateTime TimeStamp { get; set; }
+    public Scan.ScanStatus Status { get; set; }
+    
+}
