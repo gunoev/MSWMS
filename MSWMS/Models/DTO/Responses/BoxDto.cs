24758cf671daadf6f0be13029386ab3af7caeecd
movlad <movlgunoev@gmail.com>
2025-11-21 17:19:27 +0200

    Add `HasShipmentEvents` property to `BoxDto` to indicate shipment event presence

--- MSWMS/Models/DTO/Responses/BoxDto.cs
+++ MSWMS/Models/DTO/Responses/BoxDto.cs
@@ -8,4 +8,5 @@ public class BoxDto
     public int Quantity { get; set; }
     public int UserId { get; set; }
     public string Username { get; set; }
+    public bool HasShipmentEvents { get; set; }
 }
