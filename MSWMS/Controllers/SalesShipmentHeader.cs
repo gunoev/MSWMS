228cb8ea3f21ffeede991d68b33aeefc6c634112
movlad <movlgunoev@gmail.com>
2025-12-10 15:43:56 +0200

    Add `SalesShipmentHeader` and `TransferShipmentHeader` controllers with endpoints for paginated data retrieval and detailed views.

---
+++ MSWMS/Controllers/SalesShipmentHeader.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSWMS.Entities.External;
+
+namespace MSWMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesShipmentHeader : ControllerBase
+    {
+        private readonly ExternalReadOnlyContext _context;
+
+        public SalesShipmentHeader(ExternalReadOnlyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SalesShipmentHeader
+        [HttpGet("page={page}")]
+        public async Task<ActionResult<IEnumerable<object>>> GetMikesportCoSALSalesShipmentHeader(int page = 1)
+        {
+            return await _context.MikesportCoSALSalesShipmentHeader
+                .AsSplitQuery()
+                .Select(ss => new
+                {
+                    No = ss.No,
+                    OrderNo = ss.OrderNo,
+                    SellToCustomerNo = ss.SellToCustomerNo,
+                    BillToCustomerNo = ss.BillToCustomerNo,
+                    BillToName = ss.BillToName,
+                    BillToAddress = ss.BillToAddress,
+                    ShipToName = ss.ShipToName,
+                    ShipToAddress = ss.ShipToAddress,
+                    OrderDate = ss.OrderDate,
+                    PostingDate = ss.PostingDate,
+                    ShipmentDate = ss.ShipmentDate,
+                    PostingDescription = ss.PostingDescription,
+                    LocationCode = ss.LocationCode,
+                    UserId = ss.UserId,
+                })
+                .Where(ss => ss.OrderDate >= DateTime.Now.AddMonths(-1) && 
+                             ss.LocationCode == "W01")
+                .OrderByDescending(ss => ss.PostingDate)
+                .Skip((page - 1) * 10)
+                .Take(10)
+                .ToListAsync();
+
+        }
+
+        // GET: api/SalesShipmentHeader/5
+        [HttpGet("details/{no}")]
+        public async Task<ActionResult<object>> GetMikesportCoSALSalesShipmentHeaderDetails(string no)
+        {
+            var salesShipmentHeader =
+                await _context.MikesportCoSALSalesShipmentHeader
+                    .Include(ss => ss.Lines)
+                    .Select(ss => new 
+                    {
+                        No = ss.No,
+                        OrderNo = ss.OrderNo,
+                        SellToCustomerNo = ss.SellToCustomerNo,
+                        BillToCustomerNo = ss.BillToCustomerNo,
+                        BillToName = ss.BillToName,
+                        BillToAddress = ss.BillToAddress,
+                        ShipToName = ss.ShipToName,
+                        ShipToAddress = ss.ShipToAddress,
+                        OrderDate = ss.OrderDate,
+                        PostingDate = ss.PostingDate,
+                        ShipmentDate = ss.ShipmentDate,
+                        PostingDescription = ss.PostingDescription,
+                        LocationCode = ss.LocationCode,
+                        UserId = ss.UserId,
+                        Lines = ss.Lines
+                            .Where(l => 
+                                    l.GenProdPostingGroup != "SERVICES" 
+                                        && l.Quantity > 0
+                                    )
+                            .Select(l => new 
+                        {
+                            No = l.No,
+                            VariantCode = l.VariantCode,
+                            Quantity = l.Quantity,
+                        }).ToList()
+                    })
+                    .FirstOrDefaultAsync(ss => ss.No == no);
+            
+            if (salesShipmentHeader == null)
+            {
+                return NotFound();
+            }
+
+            return salesShipmentHeader;
+        }
+
+        private bool MikesportCoSALSalesShipmentHeaderExists(string id)
+        {
+            return _context.MikesportCoSALSalesShipmentHeader.Any(e => e.No == id);
+        }
+    }
+}
---
+++ MSWMS/Controllers/TransferShipmentHeader.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSWMS.Entities.External;
+
+namespace MSWMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferShipmentHeader : ControllerBase
+    {
+        private readonly ExternalReadOnlyContext _context;
+
+        public TransferShipmentHeader(ExternalReadOnlyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TransferShipmentHeader
+        [HttpGet("page={page}")]
+        public async Task<ActionResult<IEnumerable<object>>> GetMikesportCoSALTransferShipmentHeader(int page = 1)
+        {
+            var result = await _context.MikesportCoSALTransferShipmentHeader
+                .AsSplitQuery()
+                .Select(tsh => new
+            {
+                No = tsh.No,
+                TransferFromCode = tsh.TransferFromCode,
+                TransferFromName = tsh.TransferFromName,
+                TransferToCode = tsh.TransferToCode,
+                TransferToName = tsh.TransferToName,
+                TransferOrderDate = tsh.TransferOrderDate,
+                PostingDate = tsh.PostingDate,
+                TransferOrderNo = tsh.TransferOrderNo,
+                TransferFromContact = tsh.TransferFromContact,
+                TransferToContact = tsh.TransferToContact,
+                ExternalDocumentNo = tsh.ExternalDocumentNo
+            })
+                .Where(tsh => tsh.TransferOrderDate >= DateTime.Now.AddMonths(-1)
+                && tsh.TransferFromCode == "W01")
+                .OrderByDescending(tsh => tsh.PostingDate)
+                .Skip((page - 1) * 10)
+            .Take(10)
+            .ToListAsync();
+            
+            return Ok(result);
+        }
+
+        // GET: api/TransferShipmentHeader/25TS112656
+        [HttpGet("details/{no}")]
+        public async Task<ActionResult<object>> GetMikesportCoSALTransferShipmentHeaderDetails(string no)
+        {
+            var transferShipment =
+                await _context.MikesportCoSALTransferShipmentHeader
+                    .Include(tsh => tsh.Lines)
+                    .Select(tsh => new 
+                    {
+                        No = tsh.No,
+                        TransferFromCode = tsh.TransferFromCode,
+                        TransferFromName = tsh.TransferFromName,
+                        TransferToCode = tsh.TransferToCode,
+                        TransferToName = tsh.TransferToName,
+                        TransferOrderDate = tsh.TransferOrderDate,
+                        PostingDate = tsh.PostingDate,
+                        TransferOrderNo = tsh.TransferOrderNo,
+                        TransferFromContact = tsh.TransferFromContact,
+                        TransferToContact = tsh.TransferToContact,
+                        ExternalDocumentNo = tsh.ExternalDocumentNo,
+                        Lines = tsh.Lines.Select(l => new 
+                        {
+                            ItemNo = l.ItemNo,
+                            VariantCode = l.VariantCode,
+                            Quantity = l.Quantity
+                        }).ToList()
+                    })
+                    .FirstOrDefaultAsync(tsh => tsh.No == no);
+
+            if (transferShipment == null)
+            {
+                return NotFound();
+            }
+
+            return transferShipment;
+        }
+
+        private bool MikesportCoSALTransferShipmentHeaderExists(string id)
+        {
+            return _context.MikesportCoSALTransferShipmentHeader.Any(e => e.No == id);
+        }
+    }
+}
