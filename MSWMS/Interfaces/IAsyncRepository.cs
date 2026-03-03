699701d373af35c652bab3b7e24e03224a085d50
movlad <movlgunoev@gmail.com>
2026-03-03 12:33:10 +0200

    Add `IAsyncRepository` interface for generic entity operations.

---
+++ MSWMS/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+using MSWMS.Entities;
+
+namespace MSWMS.Interfaces;
+
+public interface IAsyncRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+}
