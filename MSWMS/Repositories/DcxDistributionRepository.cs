4f34cf49d8089c797f5b8b0b8997055dc033753e
movlad <movlgunoev@gmail.com>
2026-03-04 17:23:26 +0200

    Add `DcxDistributionRepository` with methods for fetching activity lines and transfer destination location codes

---
+++ MSWMS/Repositories/DcxDistributionRepository.cs
@@ -0,0 +1,40 @@
+using System.Data.Entity;
+using MSWMS.Models;
+using MSWMS.TempModels;
+
+namespace MSWMS.Repositories;
+
+public class DcxDistributionRepository
+{
+    private readonly DCXWMSContext _context;
+
+    public DcxDistributionRepository(DCXWMSContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<List<DcxMsWarehouseActivityLine>> GetLinesByDocumentNumber(string documentNumber, int activityType = 2, int actionType = 1, CancellationToken cancellationToken = default)
+    {
+        var lines = await _context.DcxMsWarehouseActivityLine
+            .Where(al => 
+                al.No == documentNumber
+                && al.ActivityType == activityType
+                && al.ActionType == actionType)
+            .ToListAsync(cancellationToken);
+
+        return lines;
+    }
+
+    public async Task<string> GetTransferDestinationLocationCode(string documentNumber, CancellationToken cancellationToken = default)
+    {
+        return await _context.DcxMsTransferHeader
+            .Where(th => th.No == documentNumber)
+            .Select(th => th.TransferToCode)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+    
+    public Task<string> GetSalesDestinationLocationCode(string documentNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
