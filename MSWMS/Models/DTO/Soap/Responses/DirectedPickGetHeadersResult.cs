414860f951320d98c7203cfab68f3a6059b01b26
movlad <movlgunoev@gmail.com>
2026-03-05 10:26:34 +0200

    Add `DirectedPickGetHeadersResult` DTO for SOAP response handling

---
+++ MSWMS/Models/DTO/Soap/Responses/DirectedPickGetHeadersResult.cs
@@ -0,0 +1,8 @@
+namespace MSWMS.Models.DTO.Soap.Responses;
+
+public class DirectedPickGetHeadersResult
+{
+    public string No { get; set; } = string.Empty;
+    public string LocationCode { get; set; } = string.Empty;
+    public string NoOfLines { get; set; } = string.Empty;
+}
