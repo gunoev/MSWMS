72fc03883c96a520e232accc07109adfad107a53
movlad <movlgunoev@gmail.com>
2026-03-06 12:27:22 +0200

    Implement `ILocationRepository` interface and update `LocationRepository` to adhere to the interface

--- MSWMS/Repositories/LocationRepository.cs
+++ MSWMS/Repositories/LocationRepository.cs
@@ -1,9 +1,10 @@
 using System.Data.Entity;
 using MSWMS.Entities;
+using MSWMS.Repositories.Interfaces;
 
 namespace MSWMS.Repositories;
 
-public class LocationRepository(AppDbContext context)
+public class LocationRepository(AppDbContext context) : ILocationRepository
 {
     
     public async Task<Location?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
