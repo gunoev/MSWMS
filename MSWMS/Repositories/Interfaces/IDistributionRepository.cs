7af8b69849279d01f04279bae5f305b61519d864
movlad <movlgunoev@gmail.com>
2026-03-06 17:42:55 +0200

    Implement `IDistributionRepository` interface and update `DistributionRepository` to adhere to it

---
+++ MSWMS/Repositories/Interfaces/IDistributionRepository.cs
@@ -0,0 +1,18 @@
+using MSWMS.Entities.Distributions;
+
+namespace MSWMS.Repositories.Interfaces;
+
+public interface IDistributionRepository
+{
+    Task<Distribution?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Distribution>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task<Distribution> AddAsync(Distribution entity, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(Distribution entity, CancellationToken cancellationToken = default);
+
+    Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    
+    
+}
