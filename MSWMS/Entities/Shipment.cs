d9d0e8aafe1d76d5a5327ef4c3656b6babeafac0
movlad <movlgunoev@gmail.com>
2026-03-24 17:34:13 +0200

    Make `Orders` optional and add `TransportPlateNumber` field to `Shipment` entity

--- MSWMS/Entities/Shipment.cs
+++ MSWMS/Entities/Shipment.cs
@@ -1,15 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MSWMS.Entities;
 
 public class Shipment
 {
     public int Id { get; set; }
     public required User CreatedBy { get; set; }
-    public required ICollection<Order> Orders { get; set; }
+    public ICollection<Order>? Orders { get; set; }
     public required Location Origin { get; set; }
     public required Location Destination { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime Scheduled { get; set; }
     public bool IsCompleted { get; set; }
+    [MaxLength(256)]
+    public string TransportPlateNumber { get; set; } = string.Empty;
     public ShipmentStatus Status { get; set; } = ShipmentStatus.Scheduled;
     public ICollection<ShipmentEvent>? Events { get; set; }
 }
